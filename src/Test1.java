21c628b4d8f285f8f0cfb39d620b6a336fc5548a
psureshmca28 <115546208+psureshmca28@users.noreply.github.com>
2022-11-03 10:21:51 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -3,11 +3,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-		int a=30;
-=======
-		int a=20;
->>>>>>> branch 'master' of https://github.com/psureshmca28/YTest.git
+int x=40;
 		
 	}
 
