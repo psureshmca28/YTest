5389a45a97128c03fe21ab370e23efd5ec317254
Suresh Pelluru <Suresh Pelluru@DESKTOP-B4ND95H>
2022-11-03 10:38:01 +0530

    commit

--- src/Test1.java
+++ src/Test1.java
@@ -3,11 +3,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
+
 int x=80;
-=======
-int x=70;
->>>>>>> branch 'master' of https://github.com/psureshmca28/YTest.git
+
 		
 	}
 
